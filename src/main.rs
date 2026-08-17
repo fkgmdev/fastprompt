2f7a580ee0c3e3c3e9c4025eeec18cfe3bfbd297
Yaman Arat <yamanarat@gmail.com>
2026-08-17 16:53:36 +0300

    quicksave

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #![allow(unused)]
 use chrono::Local;
-use tokio::process::Command;
+use tokio::{fs::read_to_string, process::Command};
 mod format;
 
 async fn get_user() -> String {
@@ -46,15 +46,42 @@ async fn get_rust() -> Option<String> {
     }
     None
 }
+async fn get_os() -> String {
+    let os_release = read_to_string("/etc/os-release")
+        .await
+        .unwrap_or("ID=linux".to_string());
+    match os_release
+        .lines()
+        .find(|line| line.starts_with("ID="))
+        .and_then(|line| line.split('=').nth(1))
+        .unwrap_or("linux")
+    {
+        "fedora" => "󰣛".to_string(),
+        "linux" => "󰌽".to_string(),
+        "arch" => "󰣇".to_string(),
+        _ => "".to_string(),
+    }
+}
 #[tokio::main]
 async fn main() {
-    let (user, rust, format_str) = tokio::join!(get_user(), get_rust(), format::config());
+    let (user, rust, format_str, os) =
+        tokio::join!(get_user(), get_rust(), format::config(), get_os());
     let cwd = get_cwd(&user);
     let time = Local::now().format("%H:%M").to_string();
-    println!("{}", make_prompt(&user, &cwd, rust, &time, &format_str));
+    println!(
+        "{}",
+        make_prompt(&user, &cwd, rust, &time, &format_str, &os)
+    );
 }
 
-fn make_prompt(user: &str, cwd: &str, rust: Option<String>, time: &str, format: &str) -> String {
+fn make_prompt(
+    user: &str,
+    cwd: &str,
+    rust: Option<String>,
+    time: &str,
+    format: &str,
+    os: &str,
+) -> String {
     // let reset = "\x1b[0m";
     if let Some(rust) = rust {
         format
@@ -64,6 +91,7 @@ fn make_prompt(user: &str, cwd: &str, rust: Option<String>, time: &str, format: 
             .replace("$time", time)
             .replace("$cwd", cwd)
             .replace("$newline", "\n")
+            .replace("$os", os)
     } else {
         format
             .replace("$space", " ")
@@ -72,5 +100,6 @@ fn make_prompt(user: &str, cwd: &str, rust: Option<String>, time: &str, format: 
             .replace("$time", time)
             .replace("$cwd", cwd)
             .replace("$newline", "\n")
+            .replace("$os", os)
     }
 }
